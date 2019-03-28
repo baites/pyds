60976feda18310f5486b6a02ad83edc0617fb2f6
Victor Eduardo Bazterra <bazterra@gmail.com>
2019-03-28 16:41:47 -0400

    Implementing key as interval for interval tree example.

--- examples/avl_interval_tree.py
+++ examples/avl_interval_tree.py
@@ -4,23 +4,41 @@ from pyds.AVLBinarySearchTree import AVLBinarySearchTree
 import random
 
 
+class Interval:
+    """Implement interval use as key."""
+
+    def __init__(self, low, high):
+        """Constructor."""
+        self.low = low
+        self.high = high
+
+    def __eq__(self, other):
+        return self.low == other.low and\
+                self.high == self.high
+
+    def __lt__(self, other):
+        return self.low < other.low
+
+
 class Node(AVLBinarySearchTreeNode):
     """Implement avl node for implementing an interval tree."""
 
-    def __init__(self, key, high):
-        super().__init__(key)
-        self.high = high
+    def __init__(self, low, high):
+        """Constructor."""
+        super().__init__(
+            Interval(low, high)
+        )
         self.maxhigh = high
 
     def label(self):
         """Customize node label."""
-        return '[{},{}]|{}'.format(self.key, self.high, self.maxhigh)
+        return '[{},{}]|{}'.format(self.key.low, self.key.high, self.maxhigh)
 
     def update(self):
         """Customize node update the keysum."""
         # Call parent update first
         super().update()
-        self.maxhigh = self.high
+        self.maxhigh = self.key.high
         if self.left is not None:
             self.maxhigh = max(self.maxhigh, self.left.maxhigh)
         if self.right is not None:
@@ -28,7 +46,7 @@ class Node(AVLBinarySearchTreeNode):
 
     def overlap(self, interval):
         """Check if interval overlap with self."""
-        if self.high < interval.key or interval.high < self.key:
+        if self.key.high < interval.low or interval.high < self.key.low:
             return False
         return True
 
@@ -46,7 +64,7 @@ class IntervalTree(AVLBinarySearchTree):
         while node is not None \
             and not node.overlap(interval):
             if node.left is not None \
-                and node.left.maxhigh >= interval.key:
+                and node.left.maxhigh >= interval.low:
                 node = node.left
             else:
                 node = node.right
@@ -74,8 +92,8 @@ def main():
     print()
 
     # Look for intervals
-    node = tree.interval(Node(18,19))
-    print(node.key, node.high)
+    node = tree.interval(Interval(18,19))
+    print(node.key.low, node.key.high)
 
 if __name__ == "__main__":
     main()
