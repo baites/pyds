dfed7f18687322be6fed8c11aacce456ecf1baa6
Victor Eduardo Bazterra <bazterra@gmail.com>
2019-03-28 19:17:26 -0400

    Implementation of interval tree by policy.

--- examples/avl_interval_tree.py
+++ examples/avl_interval_tree.py
@@ -1,79 +1,19 @@
 
 from pyds.AVLBinarySearchTree import AVLBinarySearchTreeNode
 from pyds.AVLBinarySearchTree import AVLBinarySearchTree
+from pyds.IntervalTree import Interval, IntervalTreeNode
+from pyds.IntervalTree import IntervalTree
 import random
-
-
-class Interval:
-    """Implement interval use as key."""
-
-    def __init__(self, low, high):
-        """Constructor."""
-        self.low = low
-        self.high = high
-
-    def __eq__(self, other):
-        return self.low == other.low
-        
-    def __lt__(self, other):
-        return self.low < other.low
-
-
-class Node(AVLBinarySearchTreeNode):
-    """Implement avl node for implementing an interval tree."""
-
-    def __init__(self, low, high):
-        """Constructor."""
-        super().__init__(
-            Interval(low, high)
-        )
-        self.maxhigh = high
-
-    def label(self):
-        """Customize node label."""
-        return '[{},{}]|{}'.format(self.key.low, self.key.high, self.maxhigh)
-
-    def update(self):
-        """Customize node update the keysum."""
-        # Call parent update first
-        super().update()
-        self.maxhigh = self.key.high
-        if self.left is not None:
-            self.maxhigh = max(self.maxhigh, self.left.maxhigh)
-        if self.right is not None:
-            self.maxhigh = max(self.maxhigh, self.right.maxhigh)
-
-    def overlap(self, interval):
-        """Check if interval overlap with self."""
-        if self.key.high < interval.low or interval.high < self.key.low:
-            return False
-        return True
-
-
-class IntervalTree(AVLBinarySearchTree):
-    """Implement interval tree based on AVL."""
-
-    def __init__(self, root=None):
-        """Constructor."""
-        super().__init__(root)
-
-    def interval(self, interval):
-        """Return a node that overlap with point."""
-        node = self._root
-        while node is not None \
-            and not node.overlap(interval):
-            if node.left is not None \
-                and node.left.maxhigh >= interval.low:
-                node = node.left
-            else:
-                node = node.right
-        return node
 
 
 def main():
     """Main example."""
-    # Create the trees
-    tree = IntervalTree()
+    # Define the tree type
+    Tree = IntervalTree(
+        treetype=AVLBinarySearchTree
+    )
+
+    tree = Tree()
 
     intervals = [
         (0,3), (5,8), (6,10), (8,9), (15,23),
@@ -81,6 +21,10 @@ def main():
     ]
 
     random.shuffle(intervals)
+
+    Node = IntervalTreeNode(
+        nodetype=AVLBinarySearchTreeNode
+    )
 
     for low, high in intervals:
         tree.insert(Node(low, high))
---
+++ pyds/IntervalTree.py
@@ -0,0 +1,81 @@
+"""Implement a pure python interval tree
+   where tree implementation is specified by policy.
+"""
+
+
+class Interval:
+    """Implement interval use as key."""
+
+    def __init__(self, low, high):
+        """Constructor."""
+        self.low = low
+        self.high = high
+
+    def __eq__(self, other):
+        return self.low == other.low
+
+    def __lt__(self, other):
+        return self.low < other.low
+
+
+def IntervalTreeNode(nodetype):
+    """Create node for an interval tree."""
+
+    class _(nodetype):
+        """Implement node for an interval tree."""
+
+        def __init__(self, low, high):
+            """Constructor."""
+            super().__init__(
+                Interval(low, high)
+            )
+            self.maxhigh = high
+
+        def label(self):
+            """Customize node label."""
+            return '[{},{}]|{}'.format(
+                self.key.low, self.key.high, self.maxhigh
+            )
+
+        def update(self):
+            """Customize node update the keysum."""
+            # Call parent update first
+            super().update()
+            self.maxhigh = self.key.high
+            if self.left is not None:
+                self.maxhigh = max(self.maxhigh, self.left.maxhigh)
+            if self.right is not None:
+                self.maxhigh = max(self.maxhigh, self.right.maxhigh)
+
+        def overlap(self, interval):
+            """Check if interval overlap with self."""
+            if self.key.high < interval.low or interval.high < self.key.low:
+                return False
+            return True
+
+    return _
+
+
+def IntervalTree(treetype):
+    """Create an interval tree."""
+
+    class _(treetype):
+        """Create an interval tree."""
+
+        def __init__(self, root=None):
+            """Constructor."""
+            super().__init__(root)
+
+        def interval(self, interval):
+            """Return a node that overlap with point."""
+            node = self._root
+            while node is not None and\
+                not node.overlap(interval):
+                if node.left is not None \
+                    and node.left.maxhigh >= interval.low:
+                    node = node.left
+                else:
+                    node = node.right
+            return node
+
+    return _
